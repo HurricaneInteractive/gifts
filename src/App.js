a8a4bd36f3147071448ea98c3cf026d9b3267f9c
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2019-04-27 11:35:28 +1000

    add react-router / added pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from "react"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
+// Pages
+import Index from "./pages/index"
+import User from "./pages/user"
+import Auth from "./pages/auth"
 
 function App() {
-	return <div className="App">Hello</div>
+	return (
+		<Router className="App">
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/auth">Login</Link>
+					</li>
+					<li>
+						<Link to="/user">User</Link>
+					</li>
+				</ul>
+			</nav>
+			<Route path="/" exact component={Index} />
+			<Route path="/user" exact component={User} />
+			<Route path="/auth" exact component={Auth} />
+		</Router>
+	)
 }
 
 export default App
---
+++ src/components/layout.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Layout = ({ children }) => (
+	<div>
+		<main className="container">{children}</main>
+	</div>
+)
+
+export default Layout
